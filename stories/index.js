8cf4a3e170b8382688c57157a54a6ac134fa9e0d
kokushin <mail@kokush.in>
2017-09-18 17:32:16 +0900

    [add] project.jsx

--- stories/index.js
+++ stories/index.js
@@ -8,10 +8,12 @@ import { Button, Welcome } from '@storybook/react/demo';
 
 import Editor from '../src/components/Editor/Editor';
 import Preview from '../src/components/Preview/Preview';
+import Project from '../src/components/Project/Project';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Page', module)
   .add('Editor', () => <Editor />)
-  .add('Preview', () => <Preview />);
+  .add('Preview', () => <Preview />)
+  .add('Project', () => <Project />);
 
