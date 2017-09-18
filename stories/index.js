2bd38c4af23c39d29e469098b3956454a25048de
steelydylan <ess_president@me.com>
2017-09-18 17:21:15 +0900

    [fix] story

--- stories/index.js
+++ stories/index.js
@@ -7,9 +7,11 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 
 import Editor from '../src/components/Editor/Editor';
+import Preview from '../src/components/Preview/Preview';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('Editor', () => <Editor />);
+storiesOf('Page', module)
+  .add('Editor', () => <Editor />)
+  .add('Preview', () => <Preview />);
+
